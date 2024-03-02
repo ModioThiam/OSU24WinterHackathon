71c717e9e1d068b963ac388919bc98f4aa1c0cea
AustinOregonState <146279644+AustinJHolcomb@users.noreply.github.com>
2024-03-02 15:14:25 -0600

    book search now pulls in books with the same author and category as the user input book

--- backend/google_api.py
+++ backend/google_api.py
@@ -1,5 +1,7 @@
 import requests
 
+api_key = 'AIzaSyBzBaBPGulGARHGTkr9AMmxCsjjR50KI3U'
+
 
 def get_book_id(title: str, author: str) -> list:
     """
@@ -9,7 +11,7 @@ def get_book_id(title: str, author: str) -> list:
     :return: Google Books API book ID
     """
 
-    url = 'https://www.googleapis.com/books/v1/volumes'
+    url = f'https://www.googleapis.com/books/v1/volumes'
     params = {'q': f'intitle:{title}+inauthor:{author}', 'maxResults': 1}
 
     # Request book.json
@@ -19,7 +21,7 @@ def get_book_id(title: str, author: str) -> list:
     if response.status_code == 200:
         data = response.json()
         if 'items' in data and len(data['items']) > 0:
-            book_id = data['items'][0]['id']
+            book_id = data['items'][0]
             return book_id
         else:
             print('No book found with the specified title and author.')
@@ -29,44 +31,63 @@ def get_book_id(title: str, author: str) -> list:
         return []
 
 
-def get_related_books(book_id: str, max_results=5) -> list:
+def get_category(book):
     """
-    :param book_id: Google Books API book ID
-    :param max_results: Max number of related books
-    :return: List of related books
+    :param book: A book json
+    :return: Book category
     """
-    url = f'https://www.googleapis.com/books/v1/volumes/{book_id}/related'
-    params = {'maxResults': max_results}
-    response = requests.get(url, params=params)
 
-    # Request related books
-    if response.status_code == 200:  # Search successful
+    return book['volumeInfo']['categories'][0]
+
+
+def get_book_suggestions(author: str, category: str, max_results=5):
+    """
+    :param category: Category of user input book
+    :return: List of book json with same author and same category
+    """
+    params = {'maxResults': max_results}
+    url = f'https://www.googleapis.com/books/v1/volumes?q=inauthor:{author}+subject:{category}&key={api_key}'
+
+    try:
+        response = requests.get(url, params)
         data = response.json()
-        if 'items' in data:  # If there are related books in data.json
-            related_books = []
-            for item in data['items']:  # for book in data.json
-                book_info = {
-                    'title': item['volumeInfo']['title'],
-                    'authors': item['volumeInfo'].get('authors', []),
-                    'description': item['volumeInfo'].get('description', 'No description available')
-                }
-                related_books.append(book_info)
-            return related_books
+
+        if 'items' in data:
+            return data['items']
         else:
-            print('No related books found for the book ID:', book_id)
-            return []
-    else:
-        print('Failed to retrieve related books. Status code:', response.status_code)
-        return []
+            return None
+
+    except requests.exceptions.RequestException as e:
+        print('Error:', e)
+        return None
 
 
 def main():
-    book_id = get_book_id("The Perfect Storm", "Sebastian Junger")
-    print(book_id)
 
-    # related_books = get_related_books(first_book_id, 10)
-    # for book in related_books:
-    #     print(book['title'])
+    # Test code prints out books from same author and category
+    popular_books = {
+        "To Kill a Mockingbird": "Harper Lee",
+        "1984": "George Orwell",
+        "The Great Gatsby": "F. Scott Fitzgerald",
+        "The Catcher in the Rye": "J.D. Salinger",
+        "The Lord of the Rings": "J.R.R. Tolkien"
+    }
+
+    for title, author in popular_books.items():
+
+        title_set = set()
+        test_book = get_book_id(title, author)
+        category = get_category(test_book)
+        book_suggestions = get_book_suggestions(author, category)
+
+        for book in book_suggestions:  # Remove duplicates and sequels
+            if title not in book['volumeInfo']['title']:
+                title_set.add(book['volumeInfo']['title'])
+
+        for book_title in title_set:
+            print(book_title)
+
+        print(f"\n {'_' * 50} \n")
 
 
 if __name__ == "__main__":
