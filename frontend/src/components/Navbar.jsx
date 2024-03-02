5a948058f4029362e23a1f6ec69310fea6e6f522
Jasmine Sidhu <sidhuj@oregonstate.edu>
2024-03-01 21:58:38 -0800

    Created Navbar css and jsx files

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import './Navbar.css'; 
+
+function Navbar() {
+    return(
+        <>
+            <div className="navigation">
+                <a href="/" className="logo">
+                    <img src="images/logo.png" alt="logo" />
+                </a>
+                <nav className="navigation-menu">
+                    <ul>
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#tracker">Tracker</a></li>
+                        <li><a href="#discover">Discover</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </>
+    )
+}
+
+export default Navbar;
