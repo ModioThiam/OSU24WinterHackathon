ec4cd7d851ad965555c8bc5c16099a6215f9d67b
Jasmine Sidhu <sidhuj@oregonstate.edu>
2024-03-02 00:43:48 -0800

    Added Discover page

---
+++ frontend/src/pages/Discover.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './Discover.css';
+
+function Discover() {
+    return (
+        <>
+        <div id="discover">
+            <div className="discover-container">
+                <h1>Discover New Books</h1>
+            </div>
+        </div>
+        </>
+    )
+}
+export default Discover;
+
