c951aa80f250846c8eb468a917795ef440b196c2
Jasmine Sidhu <sidhuj@oregonstate.edu>
2024-03-01 19:36:38 -0800

    enabled cors

--- backend/server.py
+++ backend/server.py
@@ -2,14 +2,14 @@
 # Import flask and datetime module for showing date and time
 from flask import Flask
 import datetime
-# from flask_cors import CORS,cross_origin
+from flask_cors import CORS,cross_origin
 
 x = datetime.datetime.now()
  
 # Initializing flask app
 app = Flask(__name__)
  
-# cors = CORS(app)
+cors = CORS(app)
 # app.config['CORS_HEADERS'] = 'Content-Type'
 # Route for seeing a data
 @app.route('/')
