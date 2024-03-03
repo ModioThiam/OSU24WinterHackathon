eab0e06da673cf2c2388b2fa104eaccd155dbeda
AustinOregonState <146279644+AustinJHolcomb@users.noreply.github.com>
2024-03-02 18:11:49 -0600

    test book search now puts suggested book results into a dict. Suggested book keys are Title, Authors, Description, and Thumbnail

--- backend/google_api.py
+++ backend/google_api.py
@@ -42,7 +42,9 @@ def get_category(book):
 
 def get_book_suggestions(author: str, category: str, max_results=5):
     """
-    :param category: Category of user input book
+    :param author: Author of the user input book
+    :param category: Category of the user input book
+    :param max_results: Maximum number of book suggestions returned
     :return: List of book json with same author and same category
     """
     params = {'maxResults': max_results}
@@ -63,29 +65,38 @@ def get_book_suggestions(author: str, category: str, max_results=5):
 
 
 def main():
-
     # Test code prints out books from same author and category
     popular_books = {
         "To Kill a Mockingbird": "Harper Lee",
         "1984": "George Orwell",
         "The Great Gatsby": "F. Scott Fitzgerald",
         "The Catcher in the Rye": "J.D. Salinger",
-        "The Lord of the Rings": "J.R.R. Tolkien"
+        "The Lord of the Rings": "J.R.R. Tolkien",
+        "Catch-22": "Joseph Heller"
     }
 
-    for title, author in popular_books.items():
+    # Get title, author, description, and cover art of each book in dict
+    for user_book_title, user_book_author in popular_books.items():
 
-        title_set = set()
-        test_book = get_book_id(title, author)
-        category = get_category(test_book)
-        book_suggestions = get_book_suggestions(author, category)
+        suggested_books = {user_book_title: {}}
+        book_suggestions_set = set()
 
-        for book in book_suggestions:  # Remove duplicates and sequels
-            if title not in book['volumeInfo']['title']:
-                title_set.add(book['volumeInfo']['title'])
+        # Get recommended books for the current user test book
+        user_book = get_book_id(user_book_title, user_book_author)
+        user_book_category = get_category(user_book)
+        book_suggestions = get_book_suggestions(user_book_author, user_book_category)
 
-        for book_title in title_set:
-            print(book_title)
+        # Create suggested books dict
+        for book in book_suggestions:
+            if user_book_title not in book['volumeInfo']['title']:  # Remove duplicates (and some sequels)
+                suggested_books[user_book_title][book['volumeInfo']['title']] = {
+                    "Authors": book['volumeInfo']['authors'],
+                    "Description": book['volumeInfo']['description'],
+                    "Thumbnail": book['volumeInfo']['imageLinks']['thumbnail']}
+
+        # Print out suggested books dict
+        for item in suggested_books[user_book_title]:
+            print(item, suggested_books[user_book_title][item])
 
         print(f"\n {'_' * 50} \n")
 
